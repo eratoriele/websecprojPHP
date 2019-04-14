0735dff7868e2af7ad390f9b1bd1669bab64e5d2
Era Toriele <fafadaw1234@gmail.com>
2019-04-14 21:54:49 +0200

    Added captcha to the registeration, more to come later

--- register.php
+++ register.php
@@ -4,6 +4,7 @@ include "database.php";
 include "include.php";
 gen_header();
 ?>
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 <?php
 if(isset($_SESSION["name"]))
 {
@@ -11,7 +12,7 @@ if(isset($_SESSION["name"]))
 }
 else
 {
-    if(isset($_GET["username"]) and isset($_GET["password"]))
+    if(isset($_POST["username"]) && isset($_POST["password"]))
         DoLogin();
     else
         ShowLoginForm();
@@ -19,13 +20,14 @@ else
 function ShowLoginForm()
 {
     ?>
-    <form class="form-signin">
+    <form class="form-signin" method="post">
         <h2 class="form-signin-heading">Please Register</h2>
         <label for="inputUser" class="sr-only">Username</label>
         <input type="text" id="inputUser" name="username" class="form-control" placeholder="Username" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <div class="g-recaptcha" data-sitekey="6LeuJ54UAAAAAKTGoUPSwBhvH7_6gyM33SFFxSOB"></div> <br/>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
     </form>
 
 <?php
@@ -33,19 +35,28 @@ function ShowLoginForm()
 
 function DoLogin()
 {
-    $hpw = password_hash($_GET["password"], PASSWORD_DEFAULT);
+    require_once('recaptchalib.php');
 
-    global $dbh;
-    $sql_query="INSERT INTO websecproj.users (Username, HashedPassword, Groups) VALUES (:user, :hpw, '1111')";
+    $response = $_POST["g-recaptcha-response"];
+    $verify = new recaptchalib("6LeuJ54UAAAAAO58XWYTLN8iSBVM1HzD5YH0FNac", $response);
 
-    $sth=$dbh->prepare($sql_query);
+    if ($verify->isValid()){
 
-    $sth->bindParam(":user", $_GET["username"]);
-    $sth->bindParam(":hpw", $hpw);
-    $sth->execute();
-    
-    $_SESSION["name"]=$_GET["username"];
-    LoggedIn(0);
+        $hpw = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+        global $dbh;
+        $sql_query="INSERT INTO websecproj.users (Username, HashedPassword, Groups) VALUES (:user, :hpw, '1111')";
+
+        $sth=$dbh->prepare($sql_query);
+
+        $sth->bindParam(":user", $_POST["username"]);
+        $sth->bindParam(":hpw", $hpw);
+        $sth->execute();
+        
+        $_SESSION["name"]=$_POST["username"];
+        LoggedIn(0);
+
+    }
 }
 
 
