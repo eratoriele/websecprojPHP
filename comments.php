7cdf2787c2e454509dafafabb77befb9da915e58
Era Toriele <fafadaw1234@gmail.com>
2019-05-01 22:22:31 +0200

    Added images to comments

--- comments.php
+++ comments.php
@@ -26,15 +26,22 @@
             echo "Security Error";
             echo '<a href="./">Go back</a><br>';
 			exit();
+        }
+        
+        $image_dir = NULL;
+		if ($_FILES["fileToUpload"]["name"] != '') {		// upload an image if there is any provided
+			require "upload.php";
+			$image_dir = $target_file;
 		}
 
-        $sql_r="INSERT INTO websecproj.comments (PostID, PostedBy, CommentBody) VALUES (:postid, :username, :body)";
+        $sql_r="INSERT INTO websecproj.comments (PostID, PostedBy, CommentBody, Image) VALUES (:postid, :username, :body, :image)";
 		
 		$sth=$dbh->prepare($sql_r);
 
         $sth->bindParam(":postid", $_GET["PostID"]);
-		$sth->bindParam(":body", $_POST["comment"]);
 		$sth->bindParam(":username", $_SESSION["name"]);
+        $sth->bindParam(":body", $_POST["comment"]);
+        $sth->bindParam(":image", $image_dir);
         $sth->execute();
         //		var_dump($sth->errorInfo());
 
@@ -57,10 +64,11 @@
         }
     
         echo "<h1>" . htmlentities($row['PostHeader']) . "</h1>";
-        echo "<p style=\"font-size: 12px;color: #3B4D45\"> Posted by: " . htmlentities($row['PostedBy']) .
-             " on " . htmlentities($row['PostedOn']) . "</p>";
+        echo "<p style=\"font-size: 12px;color: #3B4D45\"> Posted by: <a href=\"./user_profile.php?User=" .
+            $row['PostedBy'] . "\">" . htmlentities($row['PostedBy']) .
+             "</a> on " . htmlentities($row['PostedOn']) . "</p>";
         if ($row['Image'] != NULL)
-            echo "<img height=\"400\" src=" . htmlentities($row['Image']) . "><br>";
+            echo "<img height=\"600\" src=" . htmlentities($row['Image']) . "><br>";
         echo " <p style=\"font-size: 25px\">" . htmlentities($row['PostBody']) . "</p><br><hr>";
     }
     else {
@@ -71,21 +79,31 @@
     
     $sth=$dbh->query("SELECT * FROM websecproj.comments WHERE PostID = " . $_GET["PostID"] . " ORDER BY PostedOn ASC");
 	while($row = $sth->fetch( PDO::FETCH_ASSOC)) {
-		echo "<p style=\"font-size: 11px;color: blue\"> Posted by: " . htmlentities($row['PostedBy']) . " on " . htmlentities($row['PostedOn']) . "</p>";
-		echo htmlentities($row['CommentBody']) . "<br><br><hr>";
+		echo "<p style=\"font-size: 11px;color: #3B4D45\"> Posted by: <a href=\"./user_profile.php?User=" .
+				$row['PostedBy'] . "\">" . htmlentities($row['PostedBy']) . 	
+				"</a> on " . htmlentities($row['PostedOn']) . "</p>";		// Hyper text on name to user's profile
+        
+        if ($row['Image'] != NULL)
+            echo "<img height=\"400\" src=" . htmlentities($row['Image']) . "><br>";
+
+        echo htmlentities($row['CommentBody']) . "<br><br><hr>";
+        
     }
 
     $_SESSION["csrf_token"]=hash("sha256",rand().rand());
     $action_url = "./comments.php?PostID=" . $_GET["PostID"];
 ?>
 
-<form action="<?php echo $action_url ?>" method="post">
+<form action="<?php echo $action_url ?>" method="post" enctype="multipart/form-data">
     <br> <br>
     <h1>Create a new comment:</h1>
 
     <textarea name="comment" style="width: 700px;height: 80px" maxlength="2000"></textarea> <br>
 
     <div class="g-recaptcha" data-sitekey="6LeuJ54UAAAAAKTGoUPSwBhvH7_6gyM33SFFxSOB"></div> <br/>
+
+    Select image to upload: (JPG, JPEG, PNG) (Optional) (File name shouldn't be longer than 200 chars)
+	<input type="file" name="fileToUpload" id="fileToUpload">
 
     <input type="submit" value="Make a Comment"/>
 
