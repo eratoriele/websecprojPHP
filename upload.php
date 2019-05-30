597e2649dfaabbd2dffc954b395988ce9a1e595f
eratoriele <fafadaw1234@gmail.com>
2019-05-30 19:31:05 +0200

    updated .htacess

--- upload.php
+++ upload.php
@@ -9,34 +9,23 @@
     // Doesn't matter since user won't see the name
     // of the files
 
-    $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_dir . $target_file,PATHINFO_EXTENSION));         // holds the file extension of the file
     
     // Check file size
     if ($_FILES["fileToUpload"]["size"] > 2000000) {         // 2 mb
         echo "Sorry, your file is too large.<br>";
-        $uploadOk = 0;
         echo "Sorry, your file was not uploaded.<br>";
         exit();
     } 
     
+    // If a file with same name exists, add random integers before it
+    // until it becomes unique
     if (file_exists($target_dir . $target_file)) {
-        for (; file_exists($target_dir . $target_file); $target_file = rand().$target_file);
+        for (; file_exists($target_dir . $target_file); $target_file = rand(0, 9).$target_file);
     } 
-
-    // Check if image file is a actual image or fake image
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-    } 
-    else {
-        $uploadOk = 0;
-        echo "Sorry, your file was not uploaded.<br>";
-        exit();
-    }
 
     if (strlen($target_file) >= 200) {
         echo "Name of the image should be below 200 chars<br>";
-        $uploadOk = 0;
         echo "Sorry, your file was not uploaded.<br>";
         exit();
     }
@@ -44,23 +33,20 @@
     // Allow certain file formats
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ) {
         echo "Sorry, only JPG, JPEG, PNG files are allowed.<br>";
-        $uploadOk = 0;
         echo "Sorry, your file was not uploaded.<br>";
         exit();
     }
 
     $target_file = $target_dir . $target_file;
     
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 1) {
-        // if everything is ok, try to upload file
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
-        } 
-        else {
-            echo "Sorry, there was an error uploading your file.<br>";
-            exit();
-        }
+    
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
+    } 
+    else {
+        echo "Sorry, there was an error uploading your file.<br>";
+        exit();
     }
+    
 
 ?>
